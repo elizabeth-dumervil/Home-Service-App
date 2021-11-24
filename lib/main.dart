31c5674afcc5c0d22809c19a9df307d46a476df0
unknown <elizabethdumervil@gmail.com>
2021-11-24 16:50:18 +0100

    This is the first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'pages/start.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: StartPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
